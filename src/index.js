92aeca73c919265ac0849c69fd83cd340a1fd2f3
Bastian Jorquera <bastianjorquera8@gmail.com>
2022-03-10 21:59:42 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+
+let contenidoCard = document.querySelector('.container')
+$(document).ready(function() {
+    fetch('https://api-proyecto-bsale.herokuapp.com/')
+    .then(res => res.json())
+    .then(data => {
+        section(data)
+
+    })
+    
+    
+})
+
+function section(data){
+    contenidoCard.innerHTML = '';
+    for(let valor of data){
+        contenidoCard.innerHTML += `
+        <section class="container__card">
+        <img class="container__img" src="${valor.url_image}" alt="">
+            <p class="container__title">${valor.name}</p>
+            <hr class="container__hr">
+            <div class="detalles">
+                <p class="detalles__price">$${valor.price}</p>
+                <button class="detalles__button"><img class="logo" src="/images/cart.svg" alt="icono carro de compras"></button>
+            </div>
+        </section>`;
+    }
+}
+
+
